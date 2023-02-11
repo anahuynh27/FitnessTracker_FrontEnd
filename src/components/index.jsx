3967a67b574d558774c06fdabec560e38a911c36
Ana Tran <ana.tran323@gmail.com>
2023-02-10 21:35:05 -0600

    added login and register.jsx

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,3 +2,5 @@ export { default as activities } from './activities';
 export { default as homepage } from './homepage';
 export { default as routines } from './routines';
 export { default as users } from './users';
+export { default as login } from './login';
+export { default as register } from './register';
