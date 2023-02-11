fd1d55669853ed4963f26edbe22ba2c1b0f5e0c4
Vincent Palomo <vincentpalomo@yahoo.com>
2023-02-11 17:26:37 -0600

    added more psuedo code inside api

--- src/api/api.js
+++ src/api/api.js
@@ -3,8 +3,17 @@ export const APRURL = 'https://fitness-trackr-uyp6.onrender.com/api';
 
 // export fetchRequests for all endpoints
 
+// activities endpoints
+
+// all activities
 export const fetchAllActivities = async () => {
   const res = await fetch(`${APRURL}/activities`);
   const json = await res.json();
   return json;
 };
+
+// routine_activities endpoints
+
+// routines endpoints
+
+// user endpoints
