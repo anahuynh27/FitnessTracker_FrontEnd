6fe06710f3884a3a571aba28325b9bc0382fcf48
vincent <vincentpalomo@yahoo.com>
2023-02-12 17:55:37 -0600

    rendered a table for routines page, might want to have the activities route to the activities page

--- src/components/Routines.jsx
+++ src/components/Routines.jsx
@@ -23,7 +23,58 @@ const Routines = () => {
 
   return (
     <div>
-      {routines.map((r) => {
+      <div className='overflow-x-auto'>
+        <table className='min-w-full font-serif text-sm divide-y-2 divide-gray-200'>
+          <thead>
+            <tr>
+              <th className='px-4 py-2 font-medium text-left text-gray-900 whitespace-nowrap'>
+                Name
+              </th>
+              <th className='px-4 py-2 font-medium text-left text-gray-900 whitespace-nowrap'>
+                Routine
+              </th>
+              <th className='px-4 py-2 font-medium text-left text-gray-900 whitespace-nowrap'>
+                Goal
+              </th>
+              <th className='px-4 py-2 font-medium text-left text-gray-900 whitespace-nowrap'>
+                Activities
+              </th>
+              <th className='px-4 py-2 font-medium text-left text-gray-900 whitespace-nowrap'>
+                Description
+              </th>
+            </tr>
+          </thead>
+
+          <tbody className='divide-y divide-gray-200'>
+            {routines.map((r) => {
+              return (
+                <tr key={r.id}>
+                  <td className='px-4 py-2 font-medium text-gray-900 whitespace-nowrap'>
+                    {r.creatorName}
+                  </td>
+                  <td className='px-4 py-2 text-gray-700 whitespace-nowrap'>
+                    {r.name}
+                  </td>
+                  <td className='px-4 py-2 text-gray-700 whitespace-nowrap'>
+                    {r.goal}
+                  </td>
+                  <td className='px-4 py-2 text-gray-700 whitespace-nowrap'>
+                    {r.activities.map((ra) => {
+                      return <span key={ra.id}>{ra.name}</span>;
+                    })}
+                  </td>
+                  <td className='px-4 py-2 text-gray-700 whitespace-nowrap'>
+                    {r.activities.map((ra) => {
+                      return <span key={ra.id}>{ra.description}</span>;
+                    })}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+      {/* {routines.map((r) => {
         return (
           <div key={r.id}>
             <span>CREATORID: {r.creatorId}</span>
@@ -34,7 +85,7 @@ const Routines = () => {
             <span>ACTIVITIES: {r.activites}</span>
           </div>
         );
-      })}
+      })} */}
     </div>
   );
 };
