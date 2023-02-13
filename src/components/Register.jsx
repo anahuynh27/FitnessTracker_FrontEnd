372cc88c53ebb59072547fb811abfb5820e51068
Ana Tran <ana.tran323@gmail.com>
2023-02-12 18:40:50 -0600

    register needs css

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -4,8 +4,46 @@ import { fetchRegister } from '../api/api';
 const Register = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [token, setToken] = useState("");
 
-  return <div>Register</div>;
+  const handleSubmit = async(event) => {
+    event.preventDefault();
+    setUsername("");
+    setPassword("");
+
+    try {
+      const register = await fetchRegister();
+      
+      setToken(register.data.token);
+      localStorage.setItem('token', register.data.token);
+      history.pushState('/account')
+    } catch (error) {
+      console.error("Error Registering, please try again", error);
+    }
+  }
+
+  return (<div>
+  <div>Register</div>
+  <form onSubmit={handleSubmit}>
+      <label>Create Username: </label>
+      <input 
+        type="text"
+        name="username"
+        value={username}
+        onSubmit={(event) => setUsername(event.target.value)}
+      />
+      <labal>Create Password: </labal>
+      <input
+        type="password"
+        name="password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+        required
+      />
+      <button type='submit'>Create Account!</button>
+  </form>
+    </div>
+  )
 };
 
 export default Register;
