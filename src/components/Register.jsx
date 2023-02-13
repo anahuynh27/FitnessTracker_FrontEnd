e2ab8f6633b8d1d66d837e13ec7e3d6b1e2cc057
vincent <vincentpalomo@yahoo.com>
2023-02-12 20:36:25 -0600

    fixed bug with register username input

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,51 +1,55 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { fetchRegister } from '../api/api';
 
 const Register = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [token, setToken] = useState("");
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+    handleSubmit();
+  }, []);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    setUsername("");
-    setPassword("");
+    setUsername('');
+    setPassword('');
 
     try {
       const register = await fetchRegister(username, password);
-      
+
       setToken(register.data.token);
       localStorage.setItem('token', register.data.token);
-      history.pushState('/account')
+      history.pushState('/account');
     } catch (error) {
-      console.error("Error Registering, please try again", error);
+      console.error('Error Registering, please try again', error);
     }
   };
 
   return (
     <div>
       <div>Register</div>
-        <form onSubmit={handleSubmit}>
-          <label>Create Username: </label>
-          <input 
-            type='text'
-            name="username"
-            defaultValue={username}
-            onSubmit={(event) => setUsername(event.target.value)}
-            required
-          />
-          <labal>Create Password: </labal>
-          <input
-            type="password"
-            name="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-            required
-          />
-          <button type='submit'>Create Account!</button>
-        </form>
+      <form onSubmit={handleSubmit}>
+        <label>Create Username: </label>
+        <input
+          type='text'
+          name='username'
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+          required
+        />
+        <labal>Create Password: </labal>
+        <input
+          type='password'
+          name='password'
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          required
+        />
+        <button type='submit'>Create Account!</button>
+      </form>
     </div>
-  )
+  );
 };
 
 export default Register;
