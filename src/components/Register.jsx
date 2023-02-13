0112141298762c195e61f4137730ae1c4c4b575c
Ana Tran <ana.tran323@gmail.com>
2023-02-12 19:16:48 -0600

    username text working

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -6,13 +6,13 @@ const Register = () => {
   const [password, setPassword] = useState("");
   const [token, setToken] = useState("");
 
-  const handleSubmit = async(event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     setUsername("");
     setPassword("");
 
     try {
-      const register = await fetchRegister();
+      const register = await fetchRegister(username, password);
       
       setToken(register.data.token);
       localStorage.setItem('token', register.data.token);
@@ -20,28 +20,30 @@ const Register = () => {
     } catch (error) {
       console.error("Error Registering, please try again", error);
     }
-  }
+  };
 
-  return (<div>
-  <div>Register</div>
-  <form onSubmit={handleSubmit}>
-      <label>Create Username: </label>
-      <input 
-        type="text"
-        name="username"
-        value={username}
-        onSubmit={(event) => setUsername(event.target.value)}
-      />
-      <labal>Create Password: </labal>
-      <input
-        type="password"
-        name="password"
-        value={password}
-        onChange={(event) => setPassword(event.target.value)}
-        required
-      />
-      <button type='submit'>Create Account!</button>
-  </form>
+  return (
+    <div>
+      <div>Register</div>
+        <form onSubmit={handleSubmit}>
+          <label>Create Username: </label>
+          <input 
+            type='text'
+            name="username"
+            defaultValue={username}
+            onSubmit={(event) => setUsername(event.target.value)}
+            required
+          />
+          <labal>Create Password: </labal>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            required
+          />
+          <button type='submit'>Create Account!</button>
+        </form>
     </div>
   )
 };
