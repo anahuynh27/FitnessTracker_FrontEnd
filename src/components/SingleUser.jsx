eb9d04f3d3918d1333d515f46bb8d79e77aa981d
Ana Tran <ana.tran323@gmail.com>
2023-02-18 19:20:30 -0600

    page set up, issue passing in token to get just one user's routines

--- src/components/SingleUser.jsx
+++ src/components/SingleUser.jsx
@@ -1,10 +1,72 @@
-import React from 'react';
-import {fetchUsernameRoutines} from '../api/api'
+import React, {useState, useEffect} from 'react';
+import {fetchUsernameRoutines, fetchAllPublicRoutines} from '../api/api'
 
-const SingleUser = ({username}) => {
+const SingleUser = ({ token, username }) => {
+    const [routines, setRoutines] = useState([]);
+    const [usersRoutines, setUsersRoutines] = useState([]);
+
+    useEffect(() => {
+        allRoutines();
+        oneRoutines();
+      }, []);
+    
+      const allRoutines = async () => {
+        try {
+          const routines = await fetchAllPublicRoutines();
+          console.log({ routines });
+          setRoutines(routines);
+        } catch (error) {
+          console.error('error all Routines function', error);
+        }
+      };
+    
+    const oneRoutines = async () => {
+        try {
+            const users = await fetchUsernameRoutines(token, username);
+            console.log({ users });
+            setUsersRoutines(users)
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
     return (
-        <div>hello world</div>
+    <div>
+      <div className='overflow-x-auto'>
+        <table className='min-w-full font-serif text-sm divide-y-2 divide-gray-200'>
+          <thead>
+            <tr>
+              <th className='px-4 py-2 font-medium text-left text-gray-900 whitespace-nowrap'>
+                Name
+              </th>
+              <th className='px-4 py-2 font-medium text-left text-gray-900 whitespace-nowrap'>
+                Routine
+              </th>
+              <th className='px-4 py-2 font-medium text-left text-gray-900 whitespace-nowrap'>
+                Goal
+              </th>
+              <th className='px-4 py-2 font-medium text-left text-gray-900 whitespace-nowrap'>
+                Activities
+              </th>
+              <th className='px-4 py-2 font-medium text-left text-gray-900 whitespace-nowrap'>
+                Description
+              </th>
+              <th className='px-4 py-2 font-medium text-left text-gray-900 whitespace-nowrap'>
+                Count
+              </th>
+              <th className='px-4 py-2 font-medium text-left text-gray-900 whitespace-nowrap'>
+                Duration
+              </th>
+            </tr>
+            </thead>
+                    
+            <tbody>
+            
+            </tbody>
+
+        </table>
+      </div>
+    </div>
     )
 };
 
