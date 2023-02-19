432f6ed070c4c57fe783f08ed913afca4a5be46e
Ana Tran <ana.tran323@gmail.com>
2023-02-18 19:45:20 -0600

    only able to view logged in user's routines in one page

--- src/components/SingleUser.jsx
+++ src/components/SingleUser.jsx
@@ -1,13 +1,20 @@
 import React, {useState, useEffect} from 'react';
-import {fetchUsernameRoutines, fetchAllPublicRoutines} from '../api/api'
+import { fetchUsernameRoutines, fetchAllPublicRoutines } from '../api/api'
+
+//create a button on Routines.js names so when users click on the name, routes them here
+//somehow, pass in that clicked username to here
+//fetchUsernameRoutines errors due to token malformation
+//use fetchAllPublicRoutines to then filter to one user?
+
 
 const SingleUser = ({ token, username }) => {
     const [routines, setRoutines] = useState([]);
     const [usersRoutines, setUsersRoutines] = useState([]);
+    console.log(username);
 
     useEffect(() => {
         allRoutines();
-        oneRoutines();
+        // oneRoutines();
       }, []);
     
       const allRoutines = async () => {
@@ -61,7 +68,42 @@ const SingleUser = ({ token, username }) => {
             </thead>
                     
             <tbody>
-            
+                        {routines.filter((r) => (r.creatorName === username))
+                            .map((r) => {
+                                return (
+                                    <tr key={r.id}>
+                                    <td className='px-4 py-2 font-medium text-gray-900 whitespace-nowrap'>
+                                      {r.creatorName}
+                                    </td>
+                                    <td className='px-4 py-2 text-gray-700 whitespace-nowrap'>
+                                      {r.name}
+                                    </td>
+                                    <td className='px-4 py-2 text-gray-700 whitespace-nowrap'>
+                                      {r.goal}
+                                    </td>
+                                    <td className='px-4 py-2 text-gray-700 whitespace-nowrap'>
+                                      {r.activities.map((ra) => {
+                                        return <span key={ra.id}>{ra.name}</span>;
+                                      })}
+                                    </td>
+                                    <td className='px-4 py-2 text-gray-700 whitespace-nowrap'>
+                                      {r.activities.map((ra) => {
+                                        return <span key={ra.id}>{ra.description}</span>;
+                                      })}
+                                    </td>
+                                    <td className='px-4 py-2 text-gray-700 whitespace-nowrap'>
+                                      {r.activities.map((ra) => {
+                                        return <span key={ra.id}>{ra.count}</span>;
+                                      })}
+                                    </td>
+                                    <td className='px-4 py-2 text-gray-700 whitespace-nowrap'>
+                                      {r.activities.map((ra) => {
+                                        return <span key={ra.id}>{ra.duration}</span>;
+                                      })}
+                                    </td>
+                                  </tr>
+                            )
+                        })}
             </tbody>
 
         </table>
